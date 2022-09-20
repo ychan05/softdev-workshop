ff94f5a63d87a0b127782dda658a7f203dc58875
ychan2005 <ychan30@stuy.edu>
2022-09-20 13:43:31 -0400

    finish warmup2

--- 03_py/warmup2/ProblemSet2.py
+++ 03_py/warmup2/ProblemSet2.py
@@ -59,5 +59,11 @@ def array123(nums):
     return False
 
 #2-9
-def string_match(a,b):
+def string_match(a, b):
+  count = 0
+  for i in range(min(len(a),len(b))-1):
+    if (a[i:i+2] == b[i:i+2]):
+      count += 1
+  return count
+
     
