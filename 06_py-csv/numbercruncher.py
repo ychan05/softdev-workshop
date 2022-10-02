7c7f8bd4396678b93d80f07448bff8ba9d9dcd9b
ychan2005 <ychan30@stuy.edu>
2022-10-02 14:32:48 -0400

    push

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -8,16 +8,36 @@ DISCO:
 -
 QCC:
 -
+HOW THIS SCRIPT WORKS:
+
 '''
 import csv
 import random
 
-jobs = {} #init dict
+occupations = {} #init dict
 
+#parse csv
 with open('./occupations.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     next(reader) #skip heading
     for line in reader: #for every line in csv file 
-        jobs[line[0]] = float(line[1]) #occupation before percentage
+        occupations[line[0]] = float(line[1]) #occupation as key, percentage as value
 
-print(jobs)
+occupations.pop('Total') #remove last line
+
+
+def choose_job(occupations):
+    num = int(random.random()* 999)
+    for job in occupations:
+        num -= int(10 * occupations[job]) #subtract weighted percentange from rand num
+        if num <= 0:
+            return job
+
+test = {}
+for i in range(100):
+    rand = choose_job(occupations)
+    if (rand not in list(test)):
+        test[rand] = 1
+    else:
+        test[rand] += 1
+print(test)
