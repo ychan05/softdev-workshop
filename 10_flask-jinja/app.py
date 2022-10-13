47f7fe5bc1c04906b392390f6c1853843a4e107c
ychan2005 <ychan30@stuy.edu>
2022-10-13 14:06:35 -0400

    classwork

---
+++ 10_flask-jinja/app.py
@@ -0,0 +1,22 @@
+# Young Lions Crying At War:: Ameer Alnasser, Yat Long Chan, Wilson Mach
+# SoftDev pd 8
+# K10: Just Plug It In // templating
+# 2022-10-13
+
+
+from flask import Flask, render_template #Q0: What will happen if you remove render_template from this line? (log your prediction before you pull the trigger...)
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+coll = [0,1,1,2,3,5,8]
+
+@app.route("/my_foist_template") #Q1: Can all of your teammates confidently predict the URL to use to load this page?
+def test_tmplt():
+    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll) #Q2: What is the significance of each argument? Simplest, most concise answer best.
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
