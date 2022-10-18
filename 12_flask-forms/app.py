2a41ac9130eb294f5728b07fe6b5bb5c4473aec5
Yat Long Chan <yatlongc2005@gmail.com>
2022-10-17 20:31:50 -0400

    fini

--- 12_flask-forms/app.py
+++ 12_flask-forms/app.py
@@ -4,65 +4,18 @@
 #2022-10-17
 #time spent: 0.5 hours
 
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
-
-#the conventional way:
-#from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
 
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not.
-TASK: Predict which...
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests.
-Process results.
-
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
-
 @app.route("/", methods=['GET'])
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app) #prints the app running the request (Flask)
-    print("***DIAG: request obj ***")
-    print(request) #prints the server that app.py is running on
-    print("***DIAG: request.form ***")
-    #print(request.args) #returns the dictionary in which the args are stored
-    print(request.form)
-    #print("***DIAG: request.form['username']  ***")
-    #print(request.form['username']) #code returns an error when ran because arg
-                                      #argument requested doesn't exist
-    print("***DIAG: request.headers ***")
-    print(request.headers) # returns data about the webpage (operating system, web browser, etc.)
     return render_template( 'login.html' ) #run website with template
 
 
-@app.route("/auth", methods=['POST'])
+@app.route("/response", methods=['POST'])
 def authenticate():
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.form ***")
-    #print(request.args)
-    #print(request.form)
-    #print("***DIAG: request.form['username']  ***")
-    name=request.form['username'] #returns username entered
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    return render_template('response.html')  #response to a form submission
+    return render_template('response.html', user=request.form['username'], method=request.method)  #response to a form submission
 
 
 
--- 12_flask-forms/readme.md
+++ 12_flask-forms/readme.md
@@ -1,39 +1,11 @@
-  Luigi (he/they):: Yat Long Chan, Brian Wang
-  SoftDev pd 8
-  K12: Form(s) Take and Give
-  2022-10-17
-  time spent: 0.5 hours
+# Luigi (he/they) :: Yat Long Chan, Brian Wang
 
-Predictions:
-1. dspl_page
-  1. prints app that code is running on (Atom), what the code is asking for, and arguments
-     given in code to terminal
-  2. runs the code under the login.html template
+## DISCO:
+- methods must be declared in app.route before they can be used
+- in template, add `method=` in order to specify a request method to be used 
+- use request.method to access request method used
+- POST does not add data to request.args
+- existence of request.form
 
-2. authenticate
-  1. prints app that code is running on, what the code is asking for, and arguments
-     given in code to terminal
-  2. returns a website with "Waaaa hooo HAAAH"
-
-Results:
-1. dspl_page (we were wrong)
-  1. prints the app running the code (Flask), the place where the code is ran (server),
-     and the dictionary of the arguments given in webpage
-  2. runs the code under the login.html template
-
-2. authenticate (we were wrong)
-  1. prints the app running the code (Flask), the place where the code is ran (server),
-     and the dictionary of the arguments given in webpage
-  2. transitions the website into one that says "Waaaa hooo HAAAH"
-
-DISCO:
-- A HTTP 405 error is thrown when GET is excluded from method field in app.route()
-- the args dictionary is updated with user inputs
-- special chars are stored in forms dict w/ '\' 
-- the request method isn't needed to load and use webpage in browser, but stores user inputs.
-- When the methods are included in app.route(), we cannot access the values stored in request.args
-
-
-QCC:
-- What are GET and POST requests?
-- How are do they differ?
+## QCC:
+- When would GET be used over POST?
