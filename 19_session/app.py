da4a7c63584050f9e8b3ae4f58a1e57e56296443
Yat Long Chan <yatlongc2005@gmail.com>
2022-11-06 20:13:02 -0500

    push

--- 19_session/app.py
+++ 19_session/app.py
@@ -2,51 +2,39 @@
 #SoftDev pd 8
 #K19: Give me all your login info pwease
 #2022-11-03
-#time spent:  hours
+#time spent: 1.5 hours
 
 from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
-#hard-coded login
-user = "frodo"
-passwd = "baggins"
-app.secret_key="HI" #dummy secret key
+ex_user = "mykolyk"
+ex_pass = "foofoo"
+app.secret_key="HI" #dummy key
 
 @app.route('/', methods=['GET'])
 def disp_login():
-    
     if 'username' in session:
         return redirect('/auth')
     return render_template('login.html')
     
-
-
-@app.route("/auth", methods=['GET', 'POST'])
+@app.route("/auth", methods=['POST'])
 def authenticate():
     if request.method == 'GET':
-        return redirect('/response')
+        return render_template('response.html')
 
     if request.method == 'POST':
-        if request.form['username'] == user and request.form['password'] == passwd:
+        #print(request.form)
+        if request.form['username'] == ex_user and request.form["password"] == ex_pass:
             session['username']=request.form['username']
-            return redirect('/response')
-        elif request.form['username'] != user and request.form['password'] != passwd:
-            return render_template('login.html', status="Incorrect Username and Password")
-        elif request.form['username'] != user:
-            return render_template('login.html', status="Incorrect Username")
-        elif request.form['password'] != passwd:
-            return render_template('login.html', status="Incorrect Password")
+            return render_template('response.html')
+        return render_template('login.html', status="Incorrect login info")
 
-@app.route('/response')
-def response():
-    return render_template('response.html')
-    
-@app.route('/logout')
+@app.route("/logout", methods=['GET'])
 def logout():
     #wipe login info cookies
-    session.pop('username', None)
+    session.pop('username')
     return redirect('/')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
     app.run()
