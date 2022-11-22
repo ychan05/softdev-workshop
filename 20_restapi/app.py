37714fcce58e9b52e8c745adf6be3f2adc7c2958
ychan2005 <ychan30@stuy.edu>
2022-11-22 14:00:18 -0500

    push

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
 
 @app.route('/')
 def main():
-    
-    return render_template('main.html')
+    api_key = open('key_nasa.txt').read() 
+    url = 'https://api.nasa.gov/planetary/apod?api_key=' + api_key
+    result = requests.get(url).json()
+    return render_template('main.html', img_link=result.get('url'), expl=result.get('explanation'))
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
