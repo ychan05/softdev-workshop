2bb083af3025d2e5b0e6bd704125f76bb4fd9cd7
ychan2005 <ychan30@stuy.edu>
2023-04-04 17:03:05 -0400

    K 27

---
+++ 27_js/fibfac.js
@@ -0,0 +1,38 @@
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+function fact(n) {
+    if (n < 2) {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+console.log(fact(1))
+console.log(fact(2))
+console.log(fact(3))
+console.log(fact(4))
+console.log(fact(5))
+
+
+function fib(n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+console.log(fib(0))
+console.log(fib(1))
+console.log(fib(2))
+console.log(fib(3))
+console.log(fib(4))
